a57626da22c41d962009cdf86f0a49c54184b9ef
cameron-hayes <cameron.a.hayes1@outlook.com>
2024-02-13 21:07:25 -0500

    Add files via upload

---
+++ sneaky.py
@@ -0,0 +1,82 @@
+import os, discord
+from dotenv import load_dotenv
+
+load_dotenv()
+bot = discord.Bot(auto_sync_commands=True)
+token = str(os.getenv("TOKEN"))
+connections = {}
+
+discord.opus.load_opus('/opt/homebrew/Cellar/opus/1.4/lib/libopus.a')
+if not discord.opus.is_loaded():
+    raise RuntimeError('Opus failed to load')
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is ready!")
+    
+@bot.slash_command(name="hello", description="Say hello to the bot!")
+async def hello(ctx):
+    await ctx.respond("Hey!")
+
+@bot.slash_command(name="help", description="Ask for help from me!")
+async def help(ctx):
+    embed_var = discord.Embed(title="Commands you can use!", 
+                              description="**/hello** to say hi!\n**/help** to see this message", color=0x546e7a)
+    await ctx.send(embed=embed_var)
+    
+async def once_done(sink: discord.sinks, channel: discord.TextChannel, *args):
+    '''
+    once_done:
+    Coroutine that runs when recording is stopped
+    '''
+    recorded_users = [      # List of recorded users
+        f"<@{user_id}>"
+        for user_id, audio, in sink.audio_data.items()
+    ]
+    
+    await sink.vc.disconnect()  # Disconnect from voice channel
+    files = [discord.File(audio.file, f"{user_id}.{sink.encoding}") for user_id, audio in sink.audio_data.items()]  # List down the files
+    await channel.send(f"Finished recording audio for: {', '.join(recorded_users)}.", file=files)
+    
+@bot.slash_command(name="start", description="Record in my current channel!")
+async def start_recording(ctx):
+    '''
+    start_recording:
+    Action to gather connections in cache and begin recording
+    '''
+    voice = ctx.author.voice
+    
+    if not voice:
+        await ctx.respond("You must be in a voice channel to start recording!")
+        
+    vc = await voice.channel.connect()      # Connect to current voice channel
+    connections.update({ctx.guild.id: vc})  # Update cache with guild and channel
+    
+    vc.start_recording(
+        discord.sinks.WaveSink(),            # Sink to use 
+        once_done,                          # Coroutine when done recording
+        ctx.channel                         # Channel to disconnect from
+    )
+    
+    await ctx.respond("Started recording!")
+    
+@bot.command()
+async def stop_recording(ctx):
+    if ctx.guild.id in connections:  # Check if the guild is in the cache.
+        vc = connections[ctx.guild.id]
+        vc.stop_recording()  # Stop recording, and call the callback (once_done).
+        del connections[ctx.guild.id]  # Remove the guild from the cache.
+        await ctx.delete()  # And delete.
+    else:
+        await ctx.respond("I am currently not recording here.")  # Respond with this if we aren't recording.
+    
+bot.run(token)
+
+'''
+    What does SneakyBot need to do?
+    1. Join channel on slash command.
+    2. Stop recording and leave channel on slash command
+    3. Gather all users, compare them to static 'no-record' list, and list out who will be recorded
+    4. Begin recording.
+    5. Save to copies to location and then transfer to whishper
+'''
