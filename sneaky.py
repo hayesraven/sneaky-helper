d4b741d1cf8bb4c2011e4871b6d000ea3a07d4c2
Cameron Hayes <hayesraven@gmail.com>
2024-02-13 23:29:34 -0500

    Completed recording function

--- sneaky.py
+++ sneaky.py
@@ -3,10 +3,16 @@ from dotenv import load_dotenv
 import ctypes
 import ctypes.util
 
+'''
+Initial set up
+'''
 load_dotenv()
 bot = discord.Bot(auto_sync_commands=True)
 token = str(os.getenv("TOKEN"))
 connections = {}
+dont_listen = [
+    #1139938844834271242 # KenkuBot
+    ]
 
 a = ctypes.util.find_library('opus')
 discord.opus.load_opus(a)
@@ -34,16 +40,20 @@ async def once_done(sink: discord.sinks, channel: discord.TextChannel, *args):
     once_done:
     Coroutine that runs when recording is stopped
     '''
-    recorded_users = [      # List of recorded users
-        f"<@{user_id}>"
-        for user_id, audio, in sink.audio_data.items()
-    ]
-    
     await sink.vc.disconnect()  # Disconnect from voice channel
-    
-    files = [discord.File(audio.file, f"{user_id}.{sink.encoding}") for user_id, audio in sink.audio_data.items()]  # List down the files.
-    await channel.send(f"Finished recording audio for: {', '.join(recorded_users)}.", files=files)
-    
+        
+    files = []
+    for user_id, audio, in sink.audio_data.items():
+        if user_id not in dont_listen:
+            username = await bot.fetch_user(user_id)
+            with open(f"{username}.{sink.encoding}", 'wb') as f:
+                f.write(audio.file.getbuffer())
+                    
+    '''
+    TODO:
+        - Check file size for hour long sessions
+        - pass to faster
+    '''
     
 @bot.slash_command(name="start", description="Record in my current channel!")
 async def start_recording(ctx):
@@ -87,3 +97,23 @@ bot.run(token)
     4. Begin recording.
     5. Save to copies to location and then transfer to whishper
 '''
+
+
+# help links
+# this should help with merging and overlaying length of a file
+# https://github.com/Pycord-Development/pycord/blob/master/examples/audio_recording_merged.py
+
+# speaker diarization, might need it
+# https://github.com/MahmoudAshraf97/whisper-diarization
+
+# Explanation of using whisper
+# https://wandb.ai/wandb_fc/gentle-intros/reports/OpenAI-Whisper-How-to-Transcribe-Your-Audio-to-Text-for-Free-with-SRTs-VTTs---VmlldzozNDczNTI0
+
+# Another transcriber?
+# https://github.com/RyanCheddar/discord-voice-message-transcriber/blob/main/main.py
+
+# faster whisper
+# https://github.com/SYSTRAN/faster-whisper
+
+# whishper
+# https://github.com/pluja/whishper
